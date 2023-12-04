3cd949f240bd2fe4936d41b82515f35e185fd8f6
Kamran Zafar <kamranzafar4343@gmail.com>
2023-12-04 10:12:33 +0500

    created stateless widget

---
+++ practice/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: NinjaCard()));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
