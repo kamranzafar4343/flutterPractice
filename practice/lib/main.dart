e2e038b528d6ea39947b1bf92d98fe9698b06a8e
Kamran Zafar <kamranzafar4343@gmail.com>
2023-12-13 23:34:37 +0500

    using stateless widget"does'nt changes its state"

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Net Ninja App'),
-          centerTitle: true,
-          backgroundColor: Colors.amber,
-        ),
-        body: Center(
-          child: Text(
-            'hy there this is flutter practice',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              backgroundColor: Color.fromARGB(255, 241, 56, 56),
-              color: Color.fromARGB(255, 255, 255, 255),
-            ),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Net Ninja App'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: const Center(
+        child: Text(
+          'hy there this is flutter practice',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Color.fromARGB(255, 241, 56, 56),
+            color: Color.fromARGB(255, 255, 255, 255),
           ),
         ),
       ),
-    ));
+    );
+  }
+}
