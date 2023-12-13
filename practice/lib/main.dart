0caccacb526fd10d0c47e26cccae77625ad8fde0
Kamran Zafar <kamranzafar4343@gmail.com>
2023-12-13 23:29:01 +0500

    colors

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: NinjaCard()));
-}
-
-class NinjaCard extends StatelessWidget {
-  const NinjaCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Net Ninja App'),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Text(
+            'hy there this is flutter practice',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Color.fromARGB(255, 241, 56, 56),
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+      ),
+    ));
